58462cd2228cddb14698761f04b60891525978f4
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2022-10-19 23:10:41 +0530

    ubuntu installation

--- lib/installation/ubuntuinstallation.dart
+++ lib/installation/ubuntuinstallation.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../utils/textstyle.dart';
+
 class Ubuntuinstallation extends StatefulWidget {
   const Ubuntuinstallation({Key? key}) : super(key: key);
 
@@ -13,11 +15,12 @@ class _UbuntuinstallationState extends State<Ubuntuinstallation> {
     var orientation = MediaQuery.of(context).orientation;
     //size of the window
     var size = MediaQuery.of(context).size;
-    //var height = size.height;
+    var height = size.height;
     var width = size.width;
 
     return SafeArea(
       child: Scaffold(
+        backgroundColor: const Color(0xffe6f2ff),
         appBar: AppBar(
           leading: IconButton(
             icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
@@ -25,6 +28,288 @@ class _UbuntuinstallationState extends State<Ubuntuinstallation> {
           ),
           title: const Text('Ubuntu Installation'),
           centerTitle: true,
+        ),
+        body: ListView(
+          padding: const EdgeInsets.all(15.0),
+          children: [
+            const Text(
+              'Ubuntu Server Installation in Hardware',
+              style: darktext1,
+            ),
+            orientation == Orientation.portrait
+                ? Image.asset(
+              'assets/installation/ubuntuinstallation/2.png',
+              height: 250.0,
+              width: width,
+            )
+                : Image.asset(
+              'assets/installation/ubuntuinstallation/2.png',
+              height: 500.0,
+              width: width,
+            ),
+            orientation == Orientation.portrait
+                ? Image.asset(
+              'assets/installation/ubuntuinstallation/3.png',
+              height: 250.0,
+              width: width,
+            )
+                : Image.asset(
+              'assets/installation/ubuntuinstallation/3.png',
+              height: 500.0,
+              width: width,
+            ),
+            orientation == Orientation.portrait
+                ? Image.asset(
+              'assets/installation/ubuntuinstallation/4.png',
+              height: 250.0,
+              width: width,
+            )
+                : Image.asset(
+              'assets/installation/ubuntuinstallation/4.png',
+              height: 500.0,
+              width: width,
+            ),
+            orientation == Orientation.portrait
+                ? Image.asset(
+              'assets/installation/ubuntuinstallation/5.png',
+              height: 250.0,
+              width: width,
+            )
+                : Image.asset(
+              'assets/installation/ubuntuinstallation/5.png',
+              height: 500.0,
+              width: width,
+            ),
+            orientation == Orientation.portrait
+                ? Image.asset(
+              'assets/installation/ubuntuinstallation/6.png',
+              height: 250.0,
+              width: width,
+            )
+                : Image.asset(
+              'assets/installation/ubuntuinstallation/6.png',
+              height: 500.0,
+              width: width,
+            ),
+            orientation == Orientation.portrait
+                ? Image.asset(
+              'assets/installation/ubuntuinstallation/7.png',
+              height: 250.0,
+              width: width,
+            )
+                : Image.asset(
+              'assets/installation/ubuntuinstallation/7.png',
+              height: 500.0,
+              width: width,
+            ),
+            orientation == Orientation.portrait
+                ? Image.asset(
+              'assets/installation/ubuntuinstallation/8.png',
+              height: 250.0,
+              width: width,
+            )
+                : Image.asset(
+              'assets/installation/ubuntuinstallation/8.png',
+              height: 500.0,
+              width: width,
+            ),
+            orientation == Orientation.portrait
+                ? Image.asset(
+              'assets/installation/ubuntuinstallation/9.png',
+              height: 250.0,
+              width: width,
+            )
+                : Image.asset(
+              'assets/installation/ubuntuinstallation/9.png',
+              height: 500.0,
+              width: width,
+            ),
+            orientation == Orientation.portrait
+                ? Image.asset(
+              'assets/installation/ubuntuinstallation/10.png',
+              height: 250.0,
+              width: width,
+            )
+                : Image.asset(
+              'assets/installation/ubuntuinstallation/10.png',
+              height: 500.0,
+              width: width,
+            ),
+            orientation == Orientation.portrait
+                ? Image.asset(
+              'assets/installation/ubuntuinstallation/11.png',
+              height: 250.0,
+              width: width,
+            )
+                : Image.asset(
+              'assets/installation/ubuntuinstallation/11.png',
+              height: 500.0,
+              width: width,
+            ),
+            orientation == Orientation.portrait
+                ? Image.asset(
+              'assets/installation/ubuntuinstallation/12.png',
+              height: 250.0,
+              width: width,
+            )
+                : Image.asset(
+              'assets/installation/ubuntuinstallation/12.png',
+              height: 500.0,
+              width: width,
+            ),
+
+            orientation == Orientation.portrait
+                ? Image.asset(
+              'assets/installation/ubuntuinstallation/13.png',
+              height: 250.0,
+              width: width,
+            )
+                : Image.asset(
+              'assets/installation/ubuntuinstallation/13.png',
+              height: 500.0,
+              width: width,
+            ),
+            orientation == Orientation.portrait
+                ? Image.asset(
+              'assets/installation/ubuntuinstallation/14.png',
+              height: 250.0,
+              width: width,
+            )
+                : Image.asset(
+              'assets/installation/ubuntuinstallation/14.png',
+              height: 500.0,
+              width: width,
+            ),
+            orientation == Orientation.portrait
+                ? Image.asset(
+              'assets/installation/ubuntuinstallation/15.png',
+              height: 250.0,
+              width: width,
+            )
+                : Image.asset(
+              'assets/installation/ubuntuinstallation/15.png',
+              height: 500.0,
+              width: width,
+            ),
+            orientation == Orientation.portrait
+                ? Image.asset(
+              'assets/installation/ubuntuinstallation/16.png',
+              height: 250.0,
+              width: width,
+            )
+                : Image.asset(
+              'assets/installation/ubuntuinstallation/16.png',
+              height: 500.0,
+              width: width,
+            ),
+            orientation == Orientation.portrait
+                ? Image.asset(
+              'assets/installation/ubuntuinstallation/17.png',
+              height: 250.0,
+              width: width,
+            )
+                : Image.asset(
+              'assets/installation/ubuntuinstallation/17.png',
+              height: 500.0,
+              width: width,
+            ),
+            orientation == Orientation.portrait
+                ? Image.asset(
+              'assets/installation/ubuntuinstallation/18.png',
+              height: 250.0,
+              width: width,
+            )
+                : Image.asset(
+              'assets/installation/ubuntuinstallation/18.png',
+              height: 500.0,
+              width: width,
+            ),
+            orientation == Orientation.portrait
+                ? Image.asset(
+              'assets/installation/ubuntuinstallation/19.png',
+              height: 250.0,
+              width: width,
+            )
+                : Image.asset(
+              'assets/installation/ubuntuinstallation/19.png',
+              height: 500.0,
+              width: width,
+            ),
+            orientation == Orientation.portrait
+                ? Image.asset(
+              'assets/installation/ubuntuinstallation/20.png',
+              height: 250.0,
+              width: width,
+            )
+                : Image.asset(
+              'assets/installation/ubuntuinstallation/20.png',
+              height: 500.0,
+              width: width,
+            ),
+            orientation == Orientation.portrait
+                ? Image.asset(
+              'assets/installation/ubuntuinstallation/21.png',
+              height: 250.0,
+              width: width,
+            )
+                : Image.asset(
+              'assets/installation/ubuntuinstallation/21.png',
+              height: 500.0,
+              width: width,
+            ),
+            orientation == Orientation.portrait
+                ? Image.asset(
+              'assets/installation/ubuntuinstallation/22.png',
+              height: 250.0,
+              width: width,
+            )
+                : Image.asset(
+              'assets/installation/ubuntuinstallation/22.png',
+              height: 500.0,
+              width: width,
+            ),
+            orientation == Orientation.portrait
+                ? Image.asset(
+              'assets/installation/ubuntuinstallation/23.png',
+              height: 250.0,
+              width: width,
+            )
+                : Image.asset(
+              'assets/installation/ubuntuinstallation/23.png',
+              height: 500.0,
+              width: width,
+            ),
+            orientation == Orientation.portrait
+                ? Image.asset(
+              'assets/installation/ubuntuinstallation/24.png',
+              height: 250.0,
+              width: width,
+            )
+                : Image.asset(
+              'assets/installation/ubuntuinstallation/24.png',
+              height: 500.0,
+              width: width,
+            ),
+            orientation == Orientation.portrait
+                ? Image.asset(
+              'assets/installation/ubuntuinstallation/25.png',
+              height: 250.0,
+              width: width,
+            )
+                : Image.asset(
+              'assets/installation/ubuntuinstallation/25.png',
+              height: 500.0,
+              width: width,
+            ),
+
+
+
+
+
+
+
+
+          ],
         ),
       ),
     );
