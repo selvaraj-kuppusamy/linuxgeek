61c0ba503c8bec2dc5f9e0b3267c616e7d7ac1a7
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2022-11-02 10:09:03 +0530

    Why linux page completed

--- lib/screens/linux.dart
+++ lib/screens/linux.dart
@@ -82,6 +82,65 @@ class _WhylinuxState extends State<Whylinux> {
             ),
             Text('More than one function can be performed simultaneously '
                 'by dividing the CPU time intelligently.',style: normaltext1,textAlign: TextAlign.justify,),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text('Security:', style: middletext1,textAlign: TextAlign.justify,),
+            SizedBox(
+              height: 5.0,
+            ),
+            Text('It provides security in three ways namely authenticating (by assigning password and login ID), '
+                'authorization (by assigning permission to read, write and execute)'
+                ' and encryption (converts file into an unreadable format).',style: normaltext1,textAlign: TextAlign.justify,),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text('Live CD/USB:', style: middletext1,textAlign: TextAlign.justify,),
+            SizedBox(
+              height: 5.0,
+            ),
+            Text('Almost all Linux distros provide live CD/USB so that users can run/try it without installing it.',style: normaltext1,textAlign: TextAlign.justify,),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text('Graphical User Interface:', style: middletext1,textAlign: TextAlign.justify,),
+            SizedBox(
+              height: 5.0,
+            ),
+            Text('Linux is command line based OS but it can be converted to GUI based by installing packages.',style: normaltext1,textAlign: TextAlign.justify,),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text('Support\'s customized keyboard:', style: middletext1,textAlign: TextAlign.justify,),
+            SizedBox(
+              height: 5.0,
+            ),
+            Text('As it is used worldwide, hence supports different languages keyboards.',style: normaltext1,textAlign: TextAlign.justify,),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text('Application support:', style: middletext1,textAlign: TextAlign.justify,),
+            SizedBox(
+              height: 5.0,
+            ),
+            Text('It has its own software repository from where users can download and install many applications.',style: normaltext1,textAlign: TextAlign.justify,),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text('File System:', style: middletext1,textAlign: TextAlign.justify,),
+            SizedBox(
+              height: 5.0,
+            ),
+            Text('Provides hierarchical file system in which files and directories are arranged.',style: normaltext1,textAlign: TextAlign.justify,),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text('Open Source:', style: middletext1,textAlign: TextAlign.justify,),
+            SizedBox(
+              height: 5.0,
+            ),
+            Text('Linux code is freely available to all and is a community based development project.',style: normaltext1,textAlign: TextAlign.justify,),
+
 
           ],
         ),
