048d552ec2559dcd3a6b1ddb94b02c5cf1e7def7
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2022-11-05 22:32:53 +0530

    commands page

--- lib/screens/commands.dart
+++ lib/screens/commands.dart
@@ -31,6 +31,12 @@ class _CommandsState extends State<Commands> {
       'head Command',
       'tail Command',
       'tac Command',
+      'more command',
+      'less Command',
+      'su Command',
+      'id Command',
+      'useradd Command',
+      'passwd Command',
 
 
     ];
@@ -40,7 +46,7 @@ class _CommandsState extends State<Commands> {
       'The rmdir command is used to delete a directory.',
       'The ls command is used to display a list of content of a directory.',
       'The touch command is used to create empty files.',
-      'cat command is used to concatenate files and print on the standard output.t can be used to create a file.',
+      'cat command is used to concatenate files and print on the standard output.t can be used to create a file.'
           ' display content of the file, copy the content of one file to another file, and more.',
       'The cd command is used to change the current directory.',
       'The rm command is used to remove a file.',
@@ -50,6 +56,12 @@ class _CommandsState extends State<Commands> {
       'The head command is used to display the content of a file. It displays the first 10 lines of a file.',
       'The tail command is similar to the head command. The difference between both commands is that it displays the last ten lines of the file content. It is useful for reading the error message.',
       'The tac command is the reverse of cat command, as its name specified. It displays the file content in reverse order (from the last line).',
+      'The more command is quite similar to the cat command, as it is used to display the file content in the same way that the cat command does. The only difference between both commands is that, in case of larger files, the more command displays screenful output at a time.\nIn more command, the following keys are used to scroll the page:\nENTER key: To scroll down page by line.\nSpace bar: To move to the next page.\nb key: To move to the previous page.\n/ key: To search the string.',
+      'The less command is similar to the more command. It also includes some extra features such as \'adjustment in width and height of the terminal.\' Comparatively, the more command cuts the output in the width of the terminal.',
+      'The su command provides administrative access to another user. In other words, it allows access of the Linux shell to another user.',
+      'The id command is used to display the user ID (UID) and group ID (GID).',
+      'The useradd command is used to add or remove a user on a Linux server.',
+      'The passwd command is used to create and change the password for a user.',
 
     ];
     List commandsyntax = [
@@ -67,6 +79,12 @@ class _CommandsState extends State<Commands> {
       'head <file name>',
       'tail <file name>',
       'tac <file name>',
+      'more <file name> ',
+      'less <file name>',
+      'su <user name>',
+      'id',
+      'useradd  username',
+      'passwd <username>',
     ];
     List commandphoto = [
       'assets/commands/pwd.PNG',
@@ -83,8 +101,12 @@ class _CommandsState extends State<Commands> {
       'assets/commands/head.PNG',
       'assets/commands/tail.PNG',
       'assets/commands/tac.PNG',
-
-
+      'assets/commands/more.PNG',
+      'assets/commands/less.PNG',
+      'assets/commands/root.PNG',
+      'assets/commands/id.PNG',
+      'assets/commands/useradd.PNG',
+      'assets/commands/useradd.PNG'
 
 
 
